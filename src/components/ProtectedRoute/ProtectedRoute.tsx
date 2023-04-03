f7c1af1322389550bdc50d5f1b0a97e681293926
Vladimir Balashov <300wrongaway@mail.ru>
2023-04-03 17:56:53 +0300

    Refact ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,41 +1,61 @@
 import { FC, ReactNode } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Route, Redirect, useLocation } from "react-router-dom";
-import {RootState} from "../../services/reducers/rootReducer";
+// import {RootState} from "../../services/reducers/rootReducer";
+import {getCookie} from "../../services/utils"
 
 const ProtectedRoute: FC<{
   children: ReactNode;
   path: string;
   onlyUnAuth: boolean;
   exact: boolean;
-}>  = ({ children, path, onlyUnAuth = false }) => {
-  const authorized = useSelector(
-    (store: RootState) => store.authorizationReducer.authorized
-  );
+}>  = ({ onlyUnAuth, children, ...rest}) => {
+  // const authorized = useSelector(
+  //   (store: RootState) => store.authorizationReducer.authorized
+  // );
+  const isAuthorized = getCookie("accessToken");
   const location  = useLocation();
-// console.log("location", location);
 
-  return (
-    <Route
-      path={path}
-      exact={true}
-      render={() => {
-        if (authorized && onlyUnAuth) {
-          return children;
-        }
-        if (!authorized && onlyUnAuth) {
-          return  <Redirect to={{ pathname: "/login", state: { from: location } }} />;
-          // return <Redirect to="/login" />;
-        }
-        if (!authorized && !onlyUnAuth) {
-          return children;
-        }
-        if (authorized && !onlyUnAuth) {
-          return <Redirect to="/" />;
-        }
-      }}
-    />
-  );
+  // return (
+  //   <Route
+  //     path={path}
+  //     exact={true}
+  //     render={() => {
+  //       if (authorized && onlyUnAuth) {
+  //         return children;
+  //       }
+  //       if (!authorized && onlyUnAuth) {
+  //         return  <Redirect to={{ pathname: "/login", state: { from: location } }} />;
+  //         // return <Redirect to="/login" />;
+  //       }
+  //       if (!authorized && !onlyUnAuth) {
+  //         return children;
+  //       }
+  //       if (authorized && !onlyUnAuth) {
+  //         return <Redirect to="/" />;
+  //       }
+  //     }}
+  //   />
+  // );
+  if (!onlyUnAuth && isAuthorized) {
+    const { from } = location.state as any || { from: { pathname: "/" } };
+    return (
+      <Route {...rest}>
+        <Redirect to={from} />
+      </Route>
+    );
+  }
+
+  if (onlyUnAuth && !isAuthorized) {
+    return (
+      <Route {...rest}>
+        <Redirect to={{ pathname: "/login", state: { from: location } }} />
+      </Route>
+    );
+  }
+
+  return <Route {...rest}>{children}</Route>;
+
 };
 
 export default ProtectedRoute;
